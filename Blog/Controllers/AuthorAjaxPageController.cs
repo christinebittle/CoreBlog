b856cd43b7756490c9aba8ea3bd9cd67f896c599
Christine <Christine@CHRISTINE_PC>
2024-11-21 21:46:49 -0500

    AJAX examples

---
+++ Blog/Controllers/AuthorAjaxPageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class AuthorAjaxPageController : Controller
+    {
+        // These components do not connect to the data source directly
+        // Instead, the client script on each view will call the API.
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
